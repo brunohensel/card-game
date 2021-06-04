2153cc83e6f27ffd1b1eea9a987015a681a19faf
brunohensel <bruno.hensel@gmail.com>
2021-06-04 05:01:41 +0200

    :memo: refactoring project dependencies by grouping them in the dependencies.gradle. Remove some unused functions.

--- test/src/main/java/com/brunohensel/test/coroutines/Flow.kt
+++ test/src/main/java/com/brunohensel/test/coroutines/Flow.kt
@@ -1,4 +1,3 @@
-
 package com.brunohensel.test.coroutines
 
 import kotlinx.coroutines.CoroutineScope
@@ -7,35 +6,24 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 
 fun <T> Flow<T>.test(scope: CoroutineScope): TestObserver<T> = TestObserver(scope, this)
 
 class TestObserver<T>(scope: CoroutineScope, flow: Flow<T>) {
 
-  private val values = mutableListOf<T>()
+    private val values = mutableListOf<T>()
 
-  private val job: Job = scope.launch {
-    flow.collect { values.add(it) }
-  }
+    private val job: Job = scope.launch {
+        flow.collect { values.add(it) }
+    }
 
-  fun assertNoValues(): TestObserver<T> = apply { assertEquals(emptyList<T>(), values) }
+    fun assertFirst(expected: T): TestObserver<T> = apply { assertEquals(values.first(), expected) }
 
-  fun assertValues(vararg values: T): TestObserver<T> = apply { assertEquals(values.toList(), this.values) }
+    fun assertLast(expected: T): TestObserver<T> = apply { assertEquals(values.last(), expected) }
 
-  fun assertValueCount(expected: Int): TestObserver<T> = apply { assertEquals(expected, this.values.size) }
+    fun assertValueAt(index: Int, expected: T): TestObserver<T> = apply { assertEquals(values[index], expected) }
 
-  fun assertFirst(expected: T): TestObserver<T> = apply { assertEquals(values.first(), expected) }
-
-  fun assertFirst(matcher: (T) -> Boolean): TestObserver<T> = apply { assertTrue(matcher(values.first())) }
-
-  fun assertLast(expected: T): TestObserver<T> = apply { assertEquals(values.last(), expected) }
-
-  fun assertValueAt(index: Int, expected: T): TestObserver<T> = apply { assertEquals(values[index], expected) }
-
-  fun assertValueAt(index: Int, matcher: (T) -> Boolean): TestObserver<T> = apply { assertTrue(matcher(values[index])) }
-
-  fun finish() {
-    job.cancel()
-  }
+    fun finish() {
+        job.cancel()
+    }
 }
